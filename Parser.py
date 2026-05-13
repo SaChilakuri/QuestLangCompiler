e4c2a9a4a39b3aa5136d9e99c277a63fb921e614
Yellown42 <103292022+Yellown42@users.noreply.github.com>
2026-05-12 22:48:13 -0500

    In Progress (#2)
    * Implemented expression in Parser.py based off PLY documentation
    
    * Parser.py requires a UMINUS token which is missing
    
    ---------
    Co-authored-by: Ram Nallamilli <rnallamilli@imsa.edu>

--- Parser.py
+++ Parser.py
@@ -1,1 +1,42 @@
 import ply.yacc as yacc
+
+from Lexer import tokens
+
+precedence = (
+    ('left', '|', '&'),
+    ('left', '<', '>', '='),
+    ('left', '+', '-'),
+    ('left', '*', '/'),
+    ('right', 'UMINUS'),
+)
+
+def p_expressions(p):
+    '''expression : term'''
+    p[0] = p[1]
+
+def p_expression_recursive(p):
+    '''expression : expression '+' term
+
+                  | expression '-' term
+                  | expression '*' term
+                  | expression '/' term
+
+                  | expression '&' term
+                  | expression '|' term
+                  | expression '<' term
+
+                  | expression '>' term
+                  | expression '=' term'''
+    if p[2] == '+':    p[0] = p[1] + p[3]
+    elif p[2] == '-':  p[0] = p[1] - p[3]
+    elif p[2] == '*':  p[0] = p[1] * p[3]
+    elif p[2] == '/':  p[0] = p[1] / p[3]
+    elif p[2] == '&':  p[0] = p[1] and p[3]
+    elif p[2] == '|':  p[0] = p[1] or p[3]
+    elif p[2] == '<':  p[0] = p[1] < p[3]
+    elif p[2] == '>':  p[0] = p[1] > p[3]
+    elif p[2] == '=':  p[0] = p[1] == p[3]
+
+def p_expression_uminus(p):
+    '''expression : '-' expression %prec UMINUS'''
+    p[0] = -p[2]
